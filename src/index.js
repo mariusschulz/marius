0c8b42e3adc6939783c912ea9d8f7239f60e38a6
Marius Schulz <marius.schulz@me.com>
2018-12-19 21:46:34 +0000

    Switches from blue to cyan for better readability

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 #! /usr/bin/env node
 
 const boxen = require("boxen");
-const { bold, blue } = require("chalk").default;
+const { bold, cyan } = require("chalk").default;
 
 const lines = [
   bold("Marius Schulz"),
   "Front End Engineer at Facebook",
   "",
-  bold("       Web: ") + blue("https://mariusschulz.com"),
-  bold("   Twitter: ") + blue("https://twitter.com/mariusschulz"),
-  bold("    GitHub: ") + blue("https://github.com/mariusschulz"),
-  bold("  LinkedIn: ") + blue("https://linkedin.com/in/mariusschulz"),
-  bold("   Courses: ") + blue("https://egghead.io/instructors/marius-schulz")
+  bold("       Web: ") + cyan("https://mariusschulz.com"),
+  bold("   Twitter: ") + cyan("https://twitter.com/mariusschulz"),
+  bold("    GitHub: ") + cyan("https://github.com/mariusschulz"),
+  bold("  LinkedIn: ") + cyan("https://linkedin.com/in/mariusschulz"),
+  bold("   Courses: ") + cyan("https://egghead.io/instructors/marius-schulz")
 ];
 
 const message = lines.join("\n");
 
 const box = boxen(message, {
-  borderColor: "blue",
+  borderColor: "cyan",
   borderStyle: "round",
   margin: 1,
   padding: 1
